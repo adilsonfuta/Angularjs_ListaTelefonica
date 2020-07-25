acdd52a5fbc06406a765857eb041677c5a8edf06
adilsonfuta <adilsonmariafuta@gmail.com>
2020-07-25 23:49:18 +0100

    Organizando o projecto em  S ARQUITECURA SPECIFIC STYLE

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Meu Primeiro Projecto Angular
 
 #http://adilsonfuta.academiajupiter.com.br/ 
 
-!logo[()]
+## data {contem arquivo db.json usado para api restfull de teste }
+### para rodar o server db.json execute no terminal "npm server"
---
+++ javascript/controller/listaTelefonicaController.js
@@ -0,0 +1,69 @@
+      
+app.controller("listaTelefonicaController", function ($scope, $http) {
+
+    $scope.info = " Lista Telefonica ";
+    $scope.contatos = [];
+    $scope.operadoras = [
+    ];
+
+    let carregarContatos = function () {
+        $http.get("http://localhost:3000/contatos").then(
+            function (response) {
+                $scope.contatos = response.data;
+            },
+            function (response) {            
+                $scope.mensagem = "Aconteceu ALgo de Errado Nao consigo carregar nada ";
+            }
+        );
+
+        // politica da mesma origem jsonp ou CORS (Cabecalho http)
+
+    };
+
+
+    let CarregarOperadoras = function () {
+        $http.get("http://localhost:3000/operadoras").then(
+            function (response) {
+                $scope.operadoras = response.data;
+            });
+
+    };
+
+    $scope.adicionarcontato = function (contato) {
+        contato.data = new Date();
+        $http.post("http://localhost:3000/contatos", contato).then(
+            function (response) {
+                delete $scope.contato;
+                $scope.contatoForm.$setPristine();
+                carregarContatos();
+            });
+    };
+
+    $scope.ApagarContatos = function (contatos) {
+        $scope.contatos = contatos.filter(function (contato) {
+            if (!contato.selecionado) return contato;
+        });
+    };
+
+    $scope.iscontatoSel = function (contatos) {
+        return contatos.some(function (contato) {
+            return contato.selecionado;
+        });
+    };
+
+    $scope.ordenarPor = function (campo) {
+        $scope.VALOR_PARA_ORDENAR = campo;
+        $scope.SentidoOrdenar = !$scope.SentidoOrdenar;
+    }
+
+    $scope.add_classe_cor = "selecionado";
+    $scope.add_classe_negrito = "negrito";
+    $scope.falha1 = "Por favor Preencher o Nome";
+    $scope.falha2 = "Preencher devidamente com ate 10 Letras";
+    $scope.falha3 = "Por favor Preencher o Telefone";
+    $scope.falha4 = " Preencher o Telefone assim DDDXXXNNN ou 244DDDXXXNNN";
+
+    carregarContatos();
+    CarregarOperadoras();
+});
+
